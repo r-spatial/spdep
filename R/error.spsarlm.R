becba8b9f8861421124f6a947390fb4e57b8e0ef
Roger Bivand <Roger.Bivand@R-Project.org>
2018-01-28 18:31:05 +0100

    fix bug in summary.lm reporting for SLX models

--- R/error.spsarlm.R
+++ R/error.spsarlm.R
@@ -460,6 +460,7 @@ lmSLX <- function(formula, data = list(), listw, na.action, weights=NULL, zero.p
         WX <- create_WX(x, listw, zero.policy=zero.policy, prefix="lag")
         x <- cbind(x, WX)
 # 180128 Mark L. Burkey summary.lm error for SLX object
+        colnames(x) <- make.names(colnames(x))
         if (attr(mt, "intercept") == 1L) {
             lm.model <- lm(formula(paste("y ~ ", paste(colnames(x)[-1], collapse="+"))), data=as.data.frame(x), weights=weights)
         } else {
@@ -467,7 +468,7 @@ lmSLX <- function(formula, data = list(), listw, na.action, weights=NULL, zero.p
         }
         sum_lm_model <- summary.lm(lm.model, correlation = FALSE)
         mixedImps <- NULL
-	K <- ifelse(isTRUE(grep("\\(Intercept\\)",
+	K <- ifelse(isTRUE(grep("Intercept",
             names(coefficients(lm.model))[1]) == 1L), 2, 1)
         m <- length(coefficients(lm.model))
         odd <- (m%/%2) > 0
