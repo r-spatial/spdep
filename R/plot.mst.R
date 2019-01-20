788a759a0d91d2e3217f686cb86b2f0dc188e91e
Roger Bivand <roger.bivand@nhh.no>
2019-01-20 14:17:23 +0100

    docs update

--- R/plot.mst.R
+++ R/plot.mst.R
@@ -1,6 +1,6 @@
 `plot.mst` <-
 function(x, coords, label.areas=NULL,
-                     cex.circles=1, cex.labels=1, ...){
+                     cex.circles=1, cex.labels=1, add=FALSE, ...){
 ###  funcao para plotar o grafo da arvore geradora minima
 ###  vec.argem e' o vetor com os indices do vizinho de conexao
 ###       de cada area na arvore geradora minima
@@ -10,7 +10,7 @@ function(x, coords, label.areas=NULL,
    if (is.null(label.areas))
      label.areas <- as.character(1:n) 
    symbols(coords[,1], coords[,2], circles=rep(cex.circles,n),
-           inches=FALSE, xlab=" ", ylab=" ", xaxt="n", yaxt="n", ...)
+           inches=FALSE, xlab=" ", ylab=" ", xaxt="n", yaxt="n", add=add, ...)
    text(coords[,1], coords[,2], label.areas, cex=cex.labels)
    segments(coords[x[,1],1], coords[x[,1],2],
             coords[x[,2],1], coords[x[,2],2], ...)
