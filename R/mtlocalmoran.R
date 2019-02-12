25f5e0f6f812573b532bc9e724e826f525ae08ea
Roger Bivand <Roger.Bivand@nhh.no>
2019-02-12 12:34:54 +0100

    || length localmoran.sad

--- R/mtlocalmoran.R
+++ R/mtlocalmoran.R
@@ -30,7 +30,7 @@ localmoran.sad <- function (model, select, nb, glist = NULL, style = "W",
     u <- as.vector(u)
     select <- unique(as.integer(select))
     if (length(select) < 1L) stop("select too short")
-    if (any(select < 1 || select > n))
+    if (any(select < 1) || any(select > n))
         stop("select out of range")
     utu <- c(t(u) %*% u)
     p <- model$rank
