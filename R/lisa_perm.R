9aa465cbf92ae9a186039d0fdd13ccd0a952af02
Roger Bivand <roger.bivand@nhh.no>
2021-02-08 14:23:36 +0100

    add permutations

---
+++ R/lisa_perm.R
@@ -0,0 +1,153 @@
+localmoran_perm <- function(x, listw, nsim=499L, zero.policy=NULL,
+    na.action=na.fail, alternative = "greater", p.adjust.method="none",
+    mlvar=TRUE, spChk=NULL, adjust.x=FALSE, sample_Ei=TRUE) {
+    stopifnot(is.vector(x))
+    if (!inherits(listw, "listw"))
+	stop(paste(deparse(substitute(listw)), "is not a listw object"))
+    if (is.null(zero.policy))
+        zero.policy <- get("zeroPolicy", envir = .spdepOptions)
+    stopifnot(is.logical(zero.policy))
+    if (!is.null(attr(listw$neighbours, "self.included")) &&
+	attr(listw$neighbours, "self.included"))
+	stop("Self included among neighbours")
+    if (is.null(spChk)) spChk <- get.spChkOption()
+    if (spChk && !chkIDs(x, listw))
+	stop("Check of data and weights ID integrity failed")
+    if (!is.numeric(x))
+	stop(paste(deparse(substitute(x)), "is not a numeric vector"))
+    NAOK <- deparse(substitute(na.action)) == "na.pass"
+    x <- na.action(x)
+    na.act <- attr(x, "na.action")
+    rn <- attr(listw, "region.id")
+    if (!is.null(na.act)) {
+        subset <- !(1:length(listw$neighbours) %in% na.act)
+        listw <- subset(listw, subset, zero.policy=zero.policy)
+        excl <- class(na.act) == "exclude"
+    }
+    n <- length(listw$neighbours)
+    if (n != length(x))stop("Different numbers of observations")
+    res <- matrix(nrow=n, ncol=5)
+    if (alternative == "two.sided") Prname <- "Pr(z != 0)"
+    else if (alternative == "greater") Prname <- "Pr(z > 0)"
+    else Prname <- "Pr(z < 0)"
+    colnames(res) <- c("Ii", "E.Ii", "Var.Ii", "Z.Ii", Prname)
+    if (adjust.x) {
+        nc <- card(listw$neighbours) > 0L
+	xx <- mean(x[nc], na.rm=NAOK)
+    } else {
+        xx <- mean(x, na.rm=NAOK)
+    }
+    z <- x - xx 
+    lz <- lag.listw(listw, z, zero.policy=zero.policy, NAOK=NAOK)
+
+    if (mlvar) {
+        if (adjust.x) {
+            s2 <- sum(z[nc]^2, na.rm=NAOK)/sum(nc)
+        } else {
+            s2 <- sum(z^2, na.rm=NAOK)/n
+        }
+    } else {
+        if (adjust.x) {
+            s2 <- sum(z[nc]^2, na.rm=NAOK)/(sum(nc)-1) 
+        } else {
+            s2 <- sum(z^2, na.rm=NAOK)/(n-1) 
+        }
+    }
+    res[,1] <- (z/s2) * lz
+
+    crd <- card(listw$neighbours)
+    res_p <- numeric(nsim)
+    for (i in seq(along=x)) {
+        wtsi <- listw$weights[[i]]
+        zi <- z[i]
+        z_i <- z[-i]
+        crdi <- crd[i]
+        if (crdi > 0) {
+            sz_i <- matrix(sample(z_i, size=crdi*nsim, replace=TRUE),
+                ncol=crdi, nrow=nsim)
+            lz_i <- sz_i %*% wtsi
+            res_p <- (zi/s2)*lz_i
+            res[i,2] <- mean(res_p)
+            res[i,3] <- var(res_p)
+        } else {
+            res[i,2] <- as.numeric(NA)
+            res[i,3] <- as.numeric(NA)
+        }
+    }
+    if (!sample_Ei) res[,2] <- -sapply(listw$weights, sum) / (n-1)
+
+    res[,4] <- (res[,1] - res[,2]) / sqrt(res[,3])
+    if (alternative == "two.sided") pv <- 2 * pnorm(abs(res[,4]), 
+        lower.tail=FALSE)
+    else if (alternative == "greater")
+        pv <- pnorm(res[,4], lower.tail=FALSE)
+    else pv <- pnorm(res[,4])
+    res[,5] <- p.adjustSP(pv, listw$neighbours, method=p.adjust.method)
+    if (!is.null(na.act) && excl) {
+	res <- naresid(na.act, res)
+    }
+    if (!is.null(rn)) rownames(res) <- rn
+    attr(res, "call") <- match.call()
+    if (!is.null(na.act)) attr(res, "na.action") <- na.act
+    class(res) <- c("localmoran", class(res))
+    res
+}
+
+
+
+localG_perm <- function(x, listw, nsim=499, zero.policy=NULL, spChk=NULL, return_internals=FALSE) {
+    if (!inherits(listw, "listw"))
+	stop(paste(deparse(substitute(listw)), "is not a listw object"))
+    if (!is.numeric(x))
+	stop(paste(deparse(substitute(x)), "is not a numeric vector"))
+    if (is.null(zero.policy))
+        zero.policy <- get("zeroPolicy", envir = .spdepOptions)
+    stopifnot(is.logical(zero.policy))
+    stopifnot(is.vector(x))
+    if (any(is.na(x))) stop(paste("NA in ", deparse(substitute(x))))
+    n <- length(listw$neighbours)
+    if (n != length(x))stop("Different numbers of observations")
+    if (is.null(spChk)) spChk <- get.spChkOption()
+    if (spChk && !chkIDs(x, listw))
+	stop("Check of data and weights ID integrity failed")
+    gstari <- FALSE
+    if (!is.null(attr(listw$neighbours, "self.included")) &&
+	attr(listw$neighbours, "self.included")) gstari <- TRUE
+    lx <- lag.listw(listw, x, zero.policy=zero.policy)
+    x_star <- sum(x)
+    crd <- card(listw$neighbours)
+    res_p <- numeric(nsim)
+    EG <- numeric(n)
+    VG <- numeric(n)
+    for (i in seq(along=x)) {
+        wtsi <- listw$weights[[i]]
+        xi <- x[i]
+        x_i <- x[-i]
+        crdi <- crd[i]
+        if (crdi > 0) {
+            sx_i <- matrix(sample(x_i, size=crdi*nsim, replace=TRUE),
+                ncol=crdi, nrow=nsim)
+            lx_i <- sx_i %*% wtsi
+            res_p <- lx_i/(x_star-xi)
+            EG[i] <- mean(res_p)
+            VG[i] <- var(res_p)
+        } else {
+            EG[i] <- NA
+            VG[i] <- NA
+        }
+    }
+    if (gstari) {
+        G <- lx/x_star
+    } else {
+        G <- lx/(x_star-c(x))
+    }
+    res <- (G - EG)
+    res <- res / sqrt(VG)
+    if (return_internals) {
+        attr(res, "internals") <- cbind(G=G, EG=EG, VG=VG)
+    }
+    attr(res, "gstari") <- gstari
+    attr(res, "call") <- match.call()
+    class(res) <- "localG"
+    res
+}
