286a35d0a67ccaf034f6c7d87ddda7ceb16c55db
Roger Bivand <roger.bivand@nhh.no>
2021-02-13 17:33:27 +0100

    add parallel RNG handling to LISA permutations

--- R/lisa_perm.R
+++ R/lisa_perm.R
@@ -1,6 +1,6 @@
 localmoran_perm <- function(x, listw, nsim=499L, zero.policy=NULL,
     na.action=na.fail, alternative = "greater", p.adjust.method="none",
-    mlvar=TRUE, spChk=NULL, adjust.x=FALSE, sample_Ei=TRUE, iseed=1) {
+    mlvar=TRUE, spChk=NULL, adjust.x=FALSE, sample_Ei=TRUE, iseed=NULL) {
     stopifnot(is.vector(x))
     if (!inherits(listw, "listw"))
 	stop(paste(deparse(substitute(listw)), "is not a listw object"))
@@ -70,6 +70,10 @@ localmoran_perm <- function(x, listw, nsim=499L, zero.policy=NULL,
             warning("no cluster in ClusterOption, parallel set to no")
         }
     }
+    if (!is.null(iseed)) {
+        stopifnot(is.numeric(iseed))
+        stopifnot(length(iseed) == 1L)
+    }
 
     crd <- card(listw$neighbours)
     permI_int <- function(i, zi, z_i, crdi, wtsi, nsim) {
@@ -97,6 +101,7 @@ localmoran_perm <- function(x, listw, nsim=499L, zero.policy=NULL,
         assign("nsim", nsim, envir=env)
         parallel::clusterExport(cl, varlist=c("z", "crd", "lww", "nsim"),
             envir=env)
+        if (!is.null(iseed)) parallel::clusterSetRNGStream(cl, iseed = iseed)
         oo <- parallel::clusterApply(cl, x = sI, fun=lapply, function(i) {
  	    permI_int(i, z[i], z[-i], crd[i], lww[[i]], nsim)})
         out <- do.call("rbind", do.call("c", oo))
@@ -107,8 +112,12 @@ localmoran_perm <- function(x, listw, nsim=499L, zero.policy=NULL,
     } else if (parallel == "multicore") {
       if (requireNamespace("parallel", quietly = TRUE)) {
         sI <- parallel::splitIndices(n, ncpus)
+        oldRNG <- RNGkind()
+        RNGkind("L'Ecuyer-CMRG")
+        if (!is.null(iseed)) set.seed(iseed)
         oo <- parallel::mclapply(sI, FUN=lapply, function(i) {permI_int(i,
             z[i], z[-i], crd[i], lww[[i]], nsim)}, mc.cores=ncpus)
+        RNGkind(oldRNG[1])
         out <- do.call("rbind", do.call("c", oo))
       } else {
         stop("parallel not available")
@@ -145,7 +154,7 @@ localmoran_perm <- function(x, listw, nsim=499L, zero.policy=NULL,
 
 
 
-localG_perm <- function(x, listw, nsim=499, zero.policy=NULL, spChk=NULL, return_internals=FALSE) {
+localG_perm <- function(x, listw, nsim=499, zero.policy=NULL, spChk=NULL, return_internals=FALSE, iseed=NULL) {
     if (!inherits(listw, "listw"))
 	stop(paste(deparse(substitute(listw)), "is not a listw object"))
     if (!is.numeric(x))
@@ -165,32 +174,108 @@ localG_perm <- function(x, listw, nsim=499, zero.policy=NULL, spChk=NULL, return
 	attr(listw$neighbours, "self.included")) gstari <- TRUE
     lx <- lag.listw(listw, x, zero.policy=zero.policy)
     x_star <- sum(x)
-    crd <- card(listw$neighbours)
-    res_p <- numeric(nsim)
-    EG <- numeric(n)
-    VG <- numeric(n)
-    for (i in seq(along=x)) {
-        wtsi <- listw$weights[[i]]
-        xi <- x[i]
-        x_i <- x[-i]
-        crdi <- crd[i]
-        if (crdi > 0) {
-            sx_i <- matrix(sample(x_i, size=crdi*nsim, replace=TRUE),
-                ncol=crdi, nrow=nsim)
-            lx_i <- sx_i %*% wtsi
-            res_p <- lx_i/(x_star-xi)
-            EG[i] <- mean(res_p)
-            VG[i] <- var(res_p)
-        } else {
-            EG[i] <- NA
-            VG[i] <- NA
-        }
-    }
     if (gstari) {
         G <- lx/x_star
     } else {
         G <- lx/(x_star-c(x))
     }
+
+    cores <- get.coresOption()
+    if (is.null(cores)) {
+        parallel <- "no"
+    } else {
+        parallel <- ifelse (get.mcOption(), "multicore", "snow")
+    }
+    ncpus <- ifelse(is.null(cores), 1L, cores)
+    cl <- NULL
+    if (parallel == "snow") {
+        cl <- get.ClusterOption()
+        if (is.null(cl)) {
+            parallel <- "no"
+            warning("no cluster in ClusterOption, parallel set to no")
+        }
+    }
+    if (!is.null(iseed)) {
+        stopifnot(is.numeric(iseed))
+        stopifnot(length(iseed) == 1L)
+    }
+
+#    res_p <- numeric(nsim)
+#    EG <- numeric(n)
+#    VG <- numeric(n)
+#    for (i in seq(along=x)) {
+#        wtsi <- listw$weights[[i]]
+#        xi <- x[i]
+#        x_i <- x[-i]
+#        crdi <- crd[i]
+#        if (crdi > 0) {
+#            sx_i <- matrix(sample(x_i, size=crdi*nsim, replace=TRUE),
+#                ncol=crdi, nrow=nsim)
+#            lx_i <- sx_i %*% wtsi
+#            res_p <- lx_i/(x_star-xi)
+#            EG[i] <- mean(res_p)
+#            VG[i] <- var(res_p)
+#        } else {
+#            EG[i] <- NA
+#            VG[i] <- NA
+#        }
+#    }
+
+    permG_int <- function(i, xi, x_i, crdi, wtsi, nsim) {
+        if (crdi > 0) {
+            sx_i <- matrix(sample(x_i, size=crdi*nsim, replace=TRUE),
+                ncol=crdi, nrow=nsim)
+            lx_i <- sx_i %*% wtsi
+            res_p <- lx_i/(x_star-xi)
+            res <- c(mean(res_p), var(res_p))
+        } else {
+            res <- c(as.numeric(NA), as.numeric(NA))
+        }
+        res
+    }
+
+    crd <- card(listw$neighbours)
+    lww <- listw$weights
+
+    if (parallel == "snow") {
+      if (requireNamespace("parallel", quietly = TRUE)) {
+        sI <- parallel::splitIndices(n, length(cl))
+        env <- new.env()
+        assign("x", x, envir=env)
+        assign("crd", crd, envir=env)
+        assign("lww", lww, envir=env)
+        assign("nsim", nsim, envir=env)
+        parallel::clusterExport(cl, varlist=c("x", "crd", "lww", "nsim"),
+            envir=env)
+        if (!is.null(iseed)) parallel::clusterSetRNGStream(cl, iseed = iseed)
+        oo <- parallel::clusterApply(cl, x = sI, fun=lapply, function(i) {
+ 	    permG_int(i, x[i], x[-i], crd[i], lww[[i]], nsim)})
+        out <- do.call("rbind", do.call("c", oo))
+        rm(env)
+      } else {
+        stop("parallel not available")
+      }
+    } else if (parallel == "multicore") {
+      if (requireNamespace("parallel", quietly = TRUE)) {
+        sI <- parallel::splitIndices(n, ncpus)
+        oldRNG <- RNGkind()
+        RNGkind("L'Ecuyer-CMRG")
+        if (!is.null(iseed)) set.seed(iseed)
+        oo <- parallel::mclapply(sI, FUN=lapply, function(i) {permG_int(i,
+            x[i], x[-i], crd[i], lww[[i]], nsim)}, mc.cores=ncpus)
+        RNGkind(oldRNG[1])
+        out <- do.call("rbind", do.call("c", oo))
+      } else {
+        stop("parallel not available")
+      }
+    } else {
+        oo <- lapply(1:n, function(i) permG_int(i, x[i], x[-i], 
+            crd[i], lww[[i]], nsim))
+        out <- do.call("rbind", oo)
+    }
+    EG <- out[,1]
+    VG <- out[,2]
+
     res <- (G - EG)
     res <- res / sqrt(VG)
     if (return_internals) {
