b8004ac1c04dd2bfdc487113827d4cbe70c9213f
josiahparry <josiah.parry@gmail.com>
2021-11-19 15:13:00 -0500

    - Implements localC as an S3 generic with methods   - default (numeric vector)   - matrix   - formula   - list   - data.frame - Introduces argument `alternative` to `localC_perm()` with possible values `"less"` and `"greater"`. - Removed Roxygen comments from `localC.R` - Changed example to use africa conflict data from `spData`

--- R/localC.R
+++ R/localC.R
@@ -1,49 +1,9 @@
-#' @title Compute Local Geary statistic
-#'
-#' @param x a numeric vector, numeric matrix, or list. See details for more.
-#' @param listw a \code{listw} object created for example by \code{nb2listw}
-#' @param spChk hould the data vector names be checked against the spatial objects for identity integrity, TRUE, or FALSE, default NULL to use \code{get.spChkOption()}
-#'
-#' @description
-#'
-#' The Local Geary is a local adaptation of Geary's C statistic of spatial autocorrelation. The Local Geary uses squared differences to measure dissimilarity unlike the Local Moran. Low values of the Local Geary indicate positive spatial autocorrelation and large, negative.
-#'
+localC <- function(x, ...) {
+  UseMethod("localC")
+}
 
-#'
-#' @details
-#'
-#' The Local Geary can be extended to a multivariate context. When \code{x} is a numeric vector, the univariate Local Geary will be calculated. To calculate the multivariate Local Moran provide either a list or a matrix. When \code{x} is a list, each element must be a numeric vector of the same length and of the same length as the neighbours in \code{listw}. In the case that \code{x} is a matrix the number of rows must be the same as the length of the neighbours in \code{listw}.
-#'
-#' While not required in the univariate context, the standardized Local Geary is calculated. The multivariate Local Geary is \emph{always} standardized.
-#'
-#' The univariate Local Geary is calculated as \eqn{c_i = \sum_j w_{ij}(x_i - x_j)^2} and the multivariate Local Geary is calculated as \eqn{c_{k,i} = \sum_{v=1}^{k} c_{v,i}} as described in Anselin (2019).
-#'
-#' @author Josiah Parry, \email{josiah.parry@gmail.com}
-#' @references {Anselin, L. (1995), Local Indicators of Spatial Association—LISA. Geographical Analysis, 27: 93-115. \doi{10.1111/j.1538-4632.1995.tb00338.x}}
-#'
-#' {Anselin, L. (2019), A Local Indicator of Multivariate Spatial Association: Extending Geary's c. Geogr Anal, 51: 133-150. \doi{10.1111/gean.12164}}
-#'
-#' @examples
-#' columbus <- st_read(system.file("shapes/columbus.shp", package="spData")[1], quiet=TRUE)
-#' col.gal.nb <- read.gal(system.file("weights/columbus.gal", package="spData")[1])
-#' localC(columbus$CRIME, nb2listw(col.gal.nb))
-#' localC_perm(columbus$CRIME, nb2listw(col.gal.nb))
-#' @export
-localC <-  function(x, listw, spChk=NULL) {
 
-  n <- length(listw$neighbours)
-
-  # checks for list and matrix elements
-  if (is.list(x) & !length(unique(lengths(x))) == 1)
-    stop(paste("Elements of", deparse(substitute(x)), "must be of equal length"))
-
-  # maxtrix check
-  if (is.matrix(x) && !nrow(x) == n)
-    stop("Different numbers of observations")
-
-  if(is.vector(x) & is.numeric(x) & n != length(x))
-    stop("Different numbers of observations")
-
+localC.default <- function(x, ...) {
   # check listw object
   if (!inherits(listw, "listw"))
     stop(paste(deparse(substitute(listw)), "is not a listw object"))
@@ -51,42 +11,100 @@ localC <-  function(x, listw, spChk=NULL) {
   # check missing values
   if (any(is.na(x))) stop(paste("NA in ", deparse(substitute(x))))
 
-  if (is.null(spChk)) spChk <- get.spChkOption()
-  if (is.numeric(x) & spChk && !chkIDs(x, listw))
-    stop("Check of data and weights ID integrity failed")
+  localC_calc(scale(x), listw)
+}
 
-  if (inherits(x, "list")) {
-    x <- scale(Reduce(cbind, x))
+localC.formula <- function(formula, listw, data, ...) {
+  # check listw object
+  if (!inherits(listw, "listw"))
+    stop(paste(deparse(substitute(listw)), "is not a listw object."))
+
+  # check if sf object in data
+  if (inherits(data, "sf")) {
+    data[[attr(data, "sf_column")]] <- NULL
+    data <- as.data.frame(data)
   }
 
-  if (inherits(x, "matrix"))
-    res <- rowSums(apply(scale(x), 2, localC_calc, listw)) / ncol(x)
+  if (any(sapply(data, class) == "list"))
+    stop("`data` cannot contain list columns or elements.")
 
-  if (inherits(x, "numeric")) res <- localC_calc(scale(x), listw)
+  form_terms <- terms(formula, data = data)
 
-  # adding attributes as done in localG
-  attr(res, "call") <- match.call()
-  class(res) <- c("localC", "numeric")
+  if (!attr(form_terms, "response") == 0)
+    stop("`formula` must be one-sided with no response variable.")
 
-  res
+  df <- model.frame(formula, data = data)
+
+  char_cols <- colnames(df)[sapply(df, class) == "character"]
+
+  if (length(char_cols) > 0)
+    stop(paste("Formula contains character vectors:", char_cols))
+
+  rowSums(apply(scale(df), 2, localC_calc, listw)) / ncol(df)
+
+}
+
+localC.list <- function(x, listw, ...) {
+
+  if (!inherits(listw, "listw"))
+    stop(paste(deparse(substitute(listw)), "is not a listw object,"))
+
+  if (!length(unique(lengths(x))) == 1) {
+    stop("Elements of x must be of equal length.")
+  }
+
+  x <- scale(Reduce(cbind, x))
+  rowSums(apply(x, 2, localC_calc, listw)) / ncol(x)
 
 }
 
 
+localC.matrix <- function(x, listw, ...) {
+
+  if (!inherits(listw, "listw"))
+    stop(paste(deparse(substitute(listw)), "is not a listw object"))
+
+  if (inherits(x, "character")) stop("x must be a numeric matrix.")
+
+  rowSums(apply(scale(x), 2, localC_calc, listw)) / ncol(x)
+}
+
+localC.data.frame <- function(x, listw, ...) {
+
+  if (inherits(x, "sf")) {
+    x[[attr(x, "sf_column")]] <- NULL
+    x <- as.data.frame(x)
+  }
+
+  if (!all(sapply(x, class) %in% c("numeric", "integer")))
+    stop("Columns of x must be numeric.")
+
+  rowSums(apply(scale(x), 2, localC_calc, listw)) / ncol(x)
+
+}
 
 
-#' @rdname localC
-#' @export
-localC_perm <- function(x, listw, nsim = 499, spChk = NULL) {
+localC_perm <- function(x, ...) {
+  UseMethod("localC_perm")
+}
+
+localC_perm.default <- function(x, listw, nsim = 499, alternative = "less") {
+
   # checks are inherited from localC no need to implement
   obs <- localC(x, listw)
 
+  # if sf object remove geometry & cast as df
+  if (inherits(x, "sf")) {
+    x[[attr(x, "sf_column")]] <- NULL
+    x <- as.data.frame(x)
+  }
+
   if (inherits(x, "list")) {
-    x <- Reduce(cbind, x)
+    x <- scale(as.matrix(Reduce(cbind, x)))
     reps <- replicate(nsim, localC(x[sample.int(nrow(x)),], listw))
   }
 
-  if (inherits(x, "matrix")) {
+  if (inherits(x, c("matrix", "data.frame"))) {
     reps <- replicate(nsim, localC(x[sample.int(nrow(x)),], listw))
   }
 
@@ -94,18 +112,63 @@ localC_perm <- function(x, listw, nsim = 499, spChk = NULL) {
     reps <- replicate(nsim, localC(x[sample.int(length(x))], listw))
   }
 
-  pseudo_p <- (rowSums(reps <= obs)+ 1)/ (nsim + 1)
 
+  pseudo_p <- localC_p(reps, obs, alternative, nsim)
 
-  attr(res, "call") <- match.call()
-  attr(res, "pseudo-p") <- pseudo_p
-  class(res) <- c("localC", "numeric")
+  attr(obs, "call") <- match.call()
+  attr(obs, "pseudo-p") <- pseudo_p
+  class(obs) <- c("localC", "numeric")
 
-  res
+  obs
+
 }
 
+localC_perm.formula <- function(formula, listw, data,
+                                nsim = 499, alternative = "less") {
+
+  # if any data issues the localC formula method will catch it
+  obs <- localC(formula, listw, data)
+
+  # check if sf object in data
+  if (inherits(data, "sf")) {
+    data[[attr(data, "sf_column")]] <- NULL
+    data <- as.data.frame(data)
+  }
+
+  x <- scale(model.frame(formula, data = data))
+
+  reps <- replicate(nsim, localC(x[sample.int(nrow(x)),], listw))
+
+  pseudo_p <- localC_p(reps, obs, alternative, nsim)
+
+
+  attr(obs, "call") <- match.call()
+  attr(obs, "pseudo-p") <- pseudo_p
+  class(obs) <- c("localC", "numeric")
+
+  obs
+
+}
+
+
+
+
+# Local Geary Utils -------------------------------------------------------
 localC_calc <- function(x, listw) {
   xij <- sapply(listw$neighbours, FUN = function(listw) x[listw])
-  res <- mapply(function(x, j, wi) sum(wi * (j - x)^2), x, xij, listw$weights)
+  res <- mapply(function(x, j, wi) sum(wi * (j - x)^2),
+                x, xij, listw$weights,
+                USE.NAMES = FALSE)
   res
 }
+
+localC_p <- function(reps, obs, alternative, nsim) {
+
+  alternative <- match.arg(alternative, c("less", "greater"))
+
+  switch(alternative,
+         less = (rowSums(reps <= obs) + 1)/ (nsim + 1),
+         greater = (rowSums(reps >= obs) + 1)/ (nsim + 1))
+
+}
+
