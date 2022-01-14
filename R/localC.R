9803caf6aa41ac739fd9fd8bf33f77d741ad7be2
Roger Bivand <Roger.Bivand@gmail.com>
2022-01-14 09:27:14 +0100

    localC_perm multi

--- R/localC.R
+++ R/localC.R
@@ -14,7 +14,7 @@ localC.default <- function(x, listw, ..., zero.policy=NULL) {
   localC_calc(scale(x), listw, zero.policy=zero.policy)
 }
 
-localC.formula <- function(formula, listw, data, ..., zero.policy=NULL) {
+localC.formula <- function(formula, data, listw, ..., zero.policy=NULL) {
   # check listw object
   if (!inherits(listw, "listw"))
     stop(paste(deparse(substitute(listw)), "is not a listw object."))
@@ -84,14 +84,14 @@ localC.data.frame <- function(x, listw, ..., zero.policy=NULL) {
 }
 
 
-localC_perm <- function(x, ..., conditional=FALSE, zero.policy=NULL) {
+localC_perm <- function(x, ..., zero.policy=NULL) {
   UseMethod("localC_perm")
 }
 
-localC_perm.default <- function(x, listw, nsim = 499, alternative = "less",
-             ..., conditional=FALSE, zero.policy=NULL) {
+localC_perm.default <- function(x, listw, nsim = 499, alternative = "two.sided",
+             ..., zero.policy=NULL) {
 
-  alternative <- match.arg(alternative, c("less", "two.sided", "greater"))
+  alternative <- match.arg(alternative, c("two.sided", "less", "greater"))
   # checks are inherited from localC no need to implement
   obs <- localC(x, listw, zero.policy=zero.policy)
 
@@ -100,41 +100,57 @@ localC_perm.default <- function(x, listw, nsim = 499, alternative = "less",
     x[[attr(x, "sf_column")]] <- NULL
     x <- as.data.frame(x)
   }
-
+  
   if (inherits(x, "list")) {
-    x <- scale(as.matrix(Reduce(cbind, x)))
-    reps <- apply(x, 2, function(xx) localC_perm_calc(xx, listw, obs, nsim,
-      alternative=alternative, conditional=conditional,
-      zero.policy=zero.policy))
+    xorig <- as.matrix(Reduce(cbind, x))
+    x <- scale(xorig)
+    reps <- localC_perm_calc(x, listw, obs, nsim,
+      alternative=alternative, zero.policy=zero.policy)
   }
 
   if (inherits(x, c("matrix", "data.frame"))) {
-    reps <- apply(x, 2, function(xx) localC_perm_calc(xx, listw, obs, nsim,
-      alternative=alternative, conditional=conditional,
-      zero.policy=zero.policy))
+    xorig <- as.matrix(x)
+    x <- scale(xorig)
+    reps <- localC_perm_calc(x, listw, obs, nsim,
+      alternative=alternative, zero.policy=zero.policy)
   }
 
   if (is.vector(x) & is.numeric(x)) {
+    xorig <- as.matrix(x)
+    x <- scale(xorig)
     reps <- localC_perm_calc(x, listw, obs, nsim, alternative=alternative,
-      conditional=conditional, zero.policy=zero.policy)
+      zero.policy=zero.policy)
+  }
+  if (ncol(xorig) > 1L) {
+    cluster <- rep(3L, length(obs))
+    cluster[obs <= reps[, 1]] <- 1L
+    cluster[obs > reps[, 1]] <- 2L
+    cluster <- factor(cluster, levels=1:3, labels=c("Positive", "Negative",
+      "Undefined"))
+  } else {
+    a <- scale(c(xorig), scale=FALSE)
+    b <- lag(listw, a)
+    q <- rep(4L, length(a))
+    q[a > 0 & b > 0] <- 1L
+    q[a <= 0 & b > 0] <- 3L
+    q[a <= 0 & b <= 0] <- 2L
+    cluster <- factor(q, levels=1:4, labels=c("High-High", "Low-Low",
+      "Other Positive", "Negative"))
   }
 
 
-  pseudo_p <- NULL
-  if (!conditional) pseudo_p <- localC_p(reps, obs, alternative, nsim)
-  else pseudo_p <- reps
-
   attr(obs, "call") <- match.call()
-  attr(obs, "pseudo-p") <- pseudo_p
+  attr(obs, "pseudo-p") <- reps
+  attr(obs, "cluster") <- cluster
   class(obs) <- c("localC", "numeric")
 
   obs
 
 }
 
-localC_perm.formula <- function(formula, listw, data,
-                                nsim = 499, alternative = "less", ...,
-                                conditional=FALSE, zero.policy=NULL) {
+localC_perm.formula <- function(formula, data, listw,
+                                nsim = 499, alternative = "two.sided", ...,
+                                zero.policy=NULL) {
 
   alternative <- match.arg(alternative, c("less", "two.sided", "greater"))
   # if any data issues the localC formula method will catch it
@@ -146,24 +162,36 @@ localC_perm.formula <- function(formula, listw, data,
     data <- as.data.frame(data)
   }
 
-  x <- scale(model.frame(formula, data = data))
+  xorig <- model.frame(formula, data = data)
+  x <- scale()
 
-  reps <- apply(x, 2, function(xx) localC_perm_calc(xx, listw, obs, nsim,
-    alternative=alternative, conditional=conditional, zero.policy=zero.policy))
-
-  pseudo_p <- NULL
-  if (!conditional) pseudo_p <- localC_p(reps, obs, alternative, nsim)
-  else pseudo_p <- reps
-
+  reps <- localC_perm_calc(x, listw, obs, nsim, alternative=alternative,
+    zero.policy=zero.policy)
+  if (ncol(xorig) > 1L) {
+    cluster <- rep(3L, length(obs))
+    cluster[obs <= reps[, 1]] <- 1L
+    cluster[obs > reps[, 1]] <- 2L
+    cluster <- factor(cluster, levels=1:3, labels=c("Positive", "Negative",
+      "Undefined"))
+  } else {
+    a <- scale(c(xorig), scale=FALSE)
+    b <- lag(listw, a)
+    q <- rep(4L, length(a))
+    q[a > 0 & b > 0] <- 1L
+    q[a <= 0 & b > 0] <- 3L
+    q[a <= 0 & b <= 0] <- 2L
+    cluster <- factor(q, levels=1:4, labels=c("High-High", "Low-Low",
+      "Other Positive", "Negative"))
+  }
 
   attr(obs, "call") <- match.call()
-  attr(obs, "pseudo-p") <- pseudo_p
+  attr(obs, "pseudo-p") <- reps
+  attr(obs, "cluster") <- cluster
   class(obs) <- c("localC", "numeric")
 
   obs
 
 }
-
 
 
 
@@ -183,8 +211,9 @@ localC_calc <- function(x, listw, zero.policy=NULL) {
 }
 
 localC_perm_calc <- function(x, listw, obs, nsim, alternative="two.sided",
-  conditional=FALSE, zero.policy=NULL) {
-  if (conditional) {
+  zero.policy=NULL) {
+    nc <- ncol(x)
+    stopifnot(nc > 0L)
     gr <- punif((1:(nsim+1))/(nsim+1), 0, 1)
     ls <- rev(gr)
     ts <- (ifelse(gr > ls, ls, gr))*2
@@ -199,16 +228,27 @@ localC_perm_calc <- function(x, listw, obs, nsim, alternative="two.sided",
         probs <- ls
     }
     n <- length(listw$neighbours)
-    if (n != length(x))stop("Different numbers of observations")
+    if (n != nrow(x))stop("Different numbers of observations")
     crd <- card(listw$neighbours)
     permC_int <- function(i, zi, z_i, crdi, wtsi, nsim, Ci, alternative,
-      probs) {
+      probs, nc) {
       res_i <- rep(as.numeric(NA), 8)
       if (crdi > 0) {
-        sz_i <- matrix(sample(z_i, size=crdi*nsim, replace=TRUE),
-          ncol=crdi, nrow=nsim)
-        diffs <- (zi - sz_i)^2
-        res_p <- c(diffs %*% wtsi)
+        if (nc == 1L) {
+          sz_i <- matrix(sample(c(z_i), size=crdi*nsim, replace=TRUE),
+            ncol=crdi, nrow=nsim)
+          diffs <- (c(zi) - sz_i)^2
+          res_p <- c(diffs %*% wtsi)
+        } else {
+          res_ps <- matrix(NA, ncol=nc, nrow=nsim)
+          for (j in 1:nc) {
+            sz_i <- matrix(sample(z_i[,j], size=crdi*nsim, replace=TRUE),
+              ncol=crdi, nrow=nsim)
+            diffs <- (zi[,j] - sz_i)^2
+            res_ps[,j] <- c(diffs %*% wtsi)
+          }
+          res_p <- apply(res_ps, 1, mean)
+        }
 # res_p length nsim for obs i conditional draws
         res_i[1] <- mean(res_p)
         res_i[2] <- var(res_p)
@@ -229,62 +269,12 @@ localC_perm_calc <- function(x, listw, obs, nsim, alternative="two.sided",
       res_i
     }
     z <- scale(x)
-    oo <- lapply(1:n, function(i) permC_int(i, z[i], z[-i], crd[i],
-      listw$weights[[i]], nsim, obs[i], alternative, probs))
+    oo <- lapply(1:n, function(i) permC_int(i, z[i,,drop=FALSE], z[-i,], crd[i],
+      listw$weights[[i]], nsim, obs[i], alternative, probs, nc))
     res <- do.call("rbind", oo)
     colnames(res) <- c("E.Ci", "Var.Ci", "Z.Ci", Prname,
       paste0(Prname, " Sim"), "Pr(folded) Sim", "Skewness", "Kurtosis")
     res
-  } else {
-    replicate(nsim, localC(x[sample.int(length(x))], listw,
-      zero.policy=zero.policy))
-  }
 }
 
-localC_p <- function(reps, obs, alternative, nsim) {
-
-  Prname <- switch(
-    alternative,
-    two.sided = "Pr(z != E(Ci))",
-    greater = "Pr(z > E(Ci))",
-    less = "Pr(z < E(Ci))"
-  )
-  gr <- punif((1:(nsim+1))/(nsim+1), 0, 1)
-  ls <- rev(gr)
-  ts <- (ifelse(gr > ls, ls, gr))*2
-  if (alternative == "two.sided") {
-    probs <- ts
-  } else if (alternative == "greater") {
-    probs <- gr
-  } else {
-    probs <- ls
-  }
-  res <- matrix(ncol=8, nrow=nrow(reps))
-  res[,1] <- apply(reps, 1, mean)
-  res[,2] <- apply(reps, 1, var)
-  res[,3] <- (obs - res[,1])/sqrt(res[,2])
-  if (alternative == "two.sided")
-    res[,4] <- 2 * pnorm(abs(res[,3]), lower.tail=FALSE)
-  else if (alternative == "greater")
-    res[,4] <- pnorm(res[,3], lower.tail=FALSE)
-  else res[,4] <- pnorm(res[,3])
-  res[,5] <- sapply(1:nrow(reps), function(i) probs[rank(c(reps[1,], obs[i]))[(nsim + 1L)]])
-  res[,6] <- sapply(1:nrow(reps), function(i)  {
-      rnk0 <- as.integer(sum(reps[1,] >= obs[i]))
-      drnk0 <- nsim - rnk0
-      rnk <- ifelse(drnk0 < rnk0, drnk0, rnk0)
-      (rnk + 1.0) / (nsim + 1.0)
-    })
-  res[,7] <- apply(reps, 1, e1071::skewness)
-  res[,8] <- apply(reps, 1, e1071::kurtosis)
-#  switch(alternative,
-#         less = (rowSums(reps <= obs) + 1)/ (nsim + 1),
-#         greater = (rowSums(reps >= obs) + 1)/ (nsim + 1))
-
-  colnames(res) <- c("E.Ci", "Var.Ci", "Z.Ci", Prname,
-                     paste0(Prname, " Sim"), "Pr(folded) Sim", "Skewness", "Kurtosis")
-
-  res
-
-}
 
