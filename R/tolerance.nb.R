7f5c1667ccf8a8c287e18da4d8ba29867ae1a6e7
guiblanchet <guiblanchet3@gmail.com>
2022-08-29 08:56:05 -0400

    correction to tolerance.nb for fringe case problem

--- R/tolerance.nb.R
+++ R/tolerance.nb.R
@@ -1,4 +1,5 @@
-`tolerance.nb` <-function (coords, unit.angle = "degrees", max.dist, tolerance, rot.angle, plot.sites=FALSE) {
+`tolerance.nb` <-
+function (coords, unit.angle = "degrees", max.dist, tolerance, rot.angle, plot.sites=FALSE) {
 	coords <- as.matrix(coords)
 	if (missing(rot.angle)) {
 		rot.angle <- 0
@@ -27,43 +28,53 @@
 	if (plot.sites) {
 		plot(coords, pch = 19, asp = 1)
 	}
-	
+
 	dist.coords <- dist(coords)
 	angles <- find.angles(coords)
 	if (unit.angle == "degrees") {
 		angles <- (angles * 180)/pi
 	}
-	
+
 	no.good <- which((angles - tolerance) > 0, arr.ind = TRUE)
-	for (i in 1:nrow(no.good)) {
-		angles[no.good[i, 1], no.good[i, 2]] <- NA
+	if(nrow(no.good) > 0){
+		for (i in 1:nrow(no.good)) {
+			angles[no.good[i, 1], no.good[i, 2]] <- NA
+		}
 	}
-	
+
 	if (missing(max.dist)) {
 		max.dist <- max(dist.coords)
 	}else{
 		too.far <- which(as.matrix(dist.coords) > max.dist, arr.ind = TRUE)
-		for (i in 1:nrow(too.far)) {
-			angles[too.far[i, 1], too.far[i, 2]] <- NA
+		if(nrow(too.far) > 0){
+			for (i in 1:nrow(too.far)) {
+				angles[too.far[i, 1], too.far[i, 2]] <- NA
+			}
 		}
 	}
+
 	no.na <- which(!is.na(angles), arr.ind = TRUE)
-	for (i in 1:nrow(no.na)) {
-		angles[no.na[i, 1], no.na[i, 2]] <- 1
-		angles[no.na[i, 2], no.na[i, 1]] <- 1
+	if(nrow(no.na) > 0){
+		for (i in 1:nrow(no.na)) {
+			angles[no.na[i, 1], no.na[i, 2]] <- 1
+			angles[no.na[i, 2], no.na[i, 1]] <- 1
+		}
 	}
+
 	na.all <- which(is.na(angles), arr.ind = TRUE)
-	for (i in 1:nrow(na.all)) {
-		angles[na.all[i, 1], na.all[i, 2]] <- 0
+	if(nrow(na.all) > 0){
+		for (i in 1:nrow(na.all)) {
+			angles[na.all[i, 1], na.all[i, 2]] <- 0
+		}
 	}
-	
+
 	nb.obj <- mat2listw(angles)$neighbours
-	
+
 	return(nb.obj)
 }
 
 `find.angles` <-
-function (coords) 
+function (coords)
 {
     n.angles <- (nrow(coords) * (nrow(coords) - 1))/2
     angles <- vector(length = n.angles)
@@ -82,4 +93,3 @@ function (coords)
     angles <- pi/2 - atan(opp.adj)
     return(angles)
 }
-
