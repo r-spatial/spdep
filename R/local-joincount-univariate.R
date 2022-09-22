95d2ac0b9b346183a53f2a28d92b16433285e36b
Lucius Annaeus <112895848+annaeusXx@users.noreply.github.com>
2022-09-22 09:25:56 -0400

    refactor local join count univariate

--- R/local-joincount-univariate.R
+++ R/local-joincount-univariate.R
@@ -7,17 +7,23 @@
 # 5. Calculate p-values from reps
 # 6. Put table together
 # p-values are only reported for those where xi = 1L and have at least 1 neighbor with one xj == 1L value
-local_joincount_uni <- function(x, listw, nsim = 199, alternative = "two.sided") {
+local_joincount_uni <- function(fx, chosen, listw,
+                                alternative = "two.sided",
+                                nsim = 199,
+                                iseed = NULL) {
 
-  # check that x contains only 0 and 1 or TRUE FALSE
-  if (!(is.numeric(x) || is.logical(x))) stop("`x` must be an integer or logical.")
+  # check that fx is a factor with 2 levels
+  stopifnot(is.factor(fx))
+  stopifnot(
+    "`fx` must have exactly two levels" = length(levels(fx)) == 2
+  )
 
-  # make sure values are 0 or 1
-  if (!all(as.integer(x) %in% c(0L, 1L))) {
-    stop("`x` must only contain 0 and 1 or TRUE and FALSE")
-  }
+  # check chosen value
+  stopifnot(is.character(chosen))
+  stopifnot("`chosen` value is not a level of `fx`" = chosen %in% levels(fx))
 
-  # Check for x input type  and proportion of 1s vs TRUES
+
+  # retrieve weights and neighbors
   nb <- listw[["neighbours"]]
   wt <- listw[["weights"]]
 
@@ -26,10 +32,14 @@ local_joincount_uni <- function(x, listw, nsim = 199, alternative = "two.sided")
     stop("weights must be binary.")
   }
 
+  # cast `fx` to integer
+  x <- as.integer(fx == chosen)
+
   # warn if == 1 more than half of the time
   prop_occurence <- (sum(x == 1) / length(x))
   if (!prop_occurence <= 1/2) {
-    warning("`x` should occur less than half the time. \nOccurs in ",
+    warning(
+      "Chosen level of `fx` should occur less than half the time. \nOccurs in ",
             round(prop_occurence * 100, 1), "% of observations.")
   }
 
@@ -42,28 +52,57 @@ local_joincount_uni <- function(x, listw, nsim = 199, alternative = "two.sided")
   xj_index <- which(unlist(lapply(xj, function(x) any(x == 1L))) == TRUE)
   index <- intersect(xj_index, x_index)
 
-  # create replicates
-  reps <- replicate(
-    nsim,
-    (x * lag.listw(permute_listw(listw), x))[index]
-  )
+  obs <- x * lag.listw(listw, x)
 
 
-  g <- (rowSums(reps <= obs[index]) + 1) / (nsim + 1)
-  l <- (rowSums(reps >= obs[index]) + 1)/ (nsim + 1)
-  p_value <- switch(
-    alternative,
-    less = l,
-    greater = g,
-    two.sided = pmin(l, 1 - l)
-  )
+  # create new environment to pass into parallel / permBB_int function
+  env <- new.env()
+  assign("crd", card(nb), envir = env) # cardinality
+  assign("lww", wt, envir = env) # weights
+  assign("nsim", nsim, envir=env) # weights
+  assign("xi", x, envir = env) # x col
+  assign("obs", obs, envir = env) # observed values
+  varlist = ls(envir = env)
 
-  p_values <- rep(NA_real_, length(x))
-  p_values[index] <- p_value
+  permBB_int <- function(i, env) {
 
-  data.frame(
-    join_count = obs,
-    p_sim = p_values
-  )
+    crdi <- get("crd", envir = env)[i] # get the ith cardinality
+    x <- get("xi", envir = env) # get xs
+    x_i <- x[-i] # remove the observed x value for cond. permutations
+    w_i <- get("lww", envir = env)[[i]] # weights for ith element
+    nsim <- get("nsim", envir = env) # no. simulations
+    obs <- get("obs", envir = env) # observed values
+    # create matrix of replicates
+    sx_i <- matrix(sample(x_i,
+                          size = crdi * nsim,
+                          replace = TRUE),
+                   ncol = crdi,
+                   nrow = nsim)
+
+    # calculate join counts for replicates
+    res_i <- x[i] * (sx_i %*% w_i)
+
+    # return p-value ranks these will calculate p-value
+    # uses look up table approach rather than counting
+    # no. observations in each tail
+    rank(c(res_i, obs[i]))[(nsim + 1)]
+
+  }
+
+  # create look up table of probabilities
+  probs <- probs_lut("BB", nsim, alternative)
+
+  # correct for the two-sided case
+  # commenting out because this should be handled in `probs_lut()`
+  # if (alternative == "two.sided") probs <- probs / 2
+
+  p_ranks <- run_perm(permBB_int, length(x), env, iseed, varlist)
+
+  p_res <- rep(NA_real_, length(x))
+  p_res[index] <- probs[index]
+
+  res <- data.frame(obs, p_res)
+  colnames(res) <- c("BB", attr(probs, "Prname"))
+  res
 }
 
