1cb57b726c2865e7c6890017b2ca39ffa4c7367c
Roger Bivand <Roger.Bivand@gmail.com>
2024-07-26 13:32:10 +0200

    add draft beginnings of Boots LICD

---
+++ R/licd_boots.R
@@ -0,0 +1,59 @@
+licd_multi <- function(fx, listw, zero.policy=attr(listw, "zero.policy"),
+    adjust.n=TRUE, alternative = "two.sided", nsim = 199, iseed = NULL,             no_repeat_in_row=FALSE) {
+    if(!inherits(listw, "listw")) stop(paste(deparse(substitute(listw)),
+        "is not a listw object"))
+    if(!is.factor(fx)) stop(paste(deparse(substitute(fx)),
+        "is not a factor"))
+    if (any(is.na(fx))) stop("NA in factor")
+    if (is.null(zero.policy))
+        zero.policy <- get("zeroPolicy", envir = .spdepOptions)
+    stopifnot(is.logical(zero.policy))
+    n <- length(listw$neighbours)
+    if (n != length(fx)) stop("objects of different length")
+    nb <- listw$neighbours
+    crd <- card(nb)
+    if (!zero.policy && any(cards == 0))
+        stop("regions with no neighbours found")
+    ifx <- as.integer(fx)
+    k <- length(levels(fx))
+    if (k < 2) stop("must be at least two levels in factor")
+    wt <- listw$weights
+
+    env <- new.env()
+    assign("crd", card(nb), envir = env) # cardinality
+    assign("nb", nb, envir = env) # nbs
+    assign("lww", wt, envir = env) # weights
+    assign("nsim", nsim, envir=env) # sims
+    assign("xi", x, envir = env) # x col
+    assign("n", n, envir=env)
+    assign("no_repeat_in_row", no_repeat_in_row, envir=env)
+    varlist = ls(envir = env)
+
+    permBB_int <- function(i, env) {
+
+        crdi <- get("crd", envir = env)[i] # get the ith cardinality
+        x <- get("xi", envir = env) # get xs
+        x_i <- x[-i] # remove the observed x value for cond. permutations
+        xi <- x[i]
+        nb_i <- get("nb", envir = env)[[i]] # nbs for ith element
+        w_i <- get("lww", envir = env)[[i]] # weights for ith element
+        nsim <- get("nsim", envir = env) # no. simulations
+        n_i <- get("n", envir=env) - 1L
+        no_repeat_in_row <- get("no_repeat_in_row", envir=env)
+        # create matrix of replicates
+        if (no_repeat_in_row) {
+            samples <- .Call("perm_no_replace", as.integer(nsim),
+              as.integer(n_i), as.integer(crdi), PACKAGE="spdep")
+            sx_i <- matrix(x_i[samples], ncol=crdi, nrow=nsim)
+        } else {
+            sx_i <- matrix(sample(x_i, size = crdi * nsim, replace = TRUE),
+                ncol = crdi, nrow = nsim)
+        }
+
+        res_i <- x[i] * (sx_i %*% w_i)
+
+        rank(c(res_i, obs[i]))[(nsim + 1)]
+
+    }
+
+}
