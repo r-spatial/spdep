62e661c52073de6fb239078ad312b30309c55974
René Westerholt <rene.westerholt@web.de>
2026-04-18 23:46:38 +0200

    Update moran.plot.drop.R

--- R/moran.plot.drop.R
+++ R/moran.plot.drop.R
@@ -39,81 +39,86 @@ moran.plot.drop <- function(x, listw, locmoran, alpha = 0.05, adjusted_p = NULL,
     xlab <- xname
   if (is.null(ylab)) 
     ylab <- paste("spatially lagged", xname)
-  Z <- as.vector(scale(x, scale = F))
-  WZ <- lag.listw(listw, Z, zero.policy = zero.policy)
-  if (anyNA(WZ)) warning("no-neighbour observation(s) found - use zero.policy=TRUE")
+  
+  WX <- lag.listw(listw, scale(x, scale = F), zero.policy = zero.policy)
+  if (anyNA(WX)) warning("no-neighbour observation(s) found - use zero.policy=TRUE")
   if(usePadj)
     cv <- min(abs(locmoran[(which(adjusted_p <= alpha/2)), 4]))
   else
-    cv <- qnorm(1 - alpha, lower.tail = FALSE)
-  b <- ((cv * sqrt(locmoran[, 3])) + locmoran[, 2]) * var(Z) / Z
-  b2 <- ((-cv * sqrt(locmoran[, 3])) + locmoran[, 2]) * var(Z) / Z
-  b[which((Z < 0 & WZ > mean(WZ)) | (Z > 0 & WZ < mean(WZ)))] <- b2[which((Z < 0 & WZ > mean(WZ)) | (Z > 0 & WZ < mean(WZ)))]
+    cv <- abs(qnorm(1 - alpha, lower.tail = FALSE))
+
+  b <- ((cv * sqrt(locmoran[, 3])) + locmoran[, 2]) * var(x) / (x - mean(x))
+  b2 <- ((-cv * sqrt(locmoran[, 3])) + locmoran[, 2]) * var(x) / (x - mean(x))
+  b[which((x < mean(x) & WX > mean(WX)) | (x > mean(x) & WX < mean(WX)))] <- b2[which((x < mean(x) & WX > mean(WX)) | (x > mean(x) & WX < mean(WX)))]
   
   if(significant) {
-    x_q1 <- Z[which(Z > 0 & WZ > mean(WZ) & locmoran[, 4] >= cv)]
-    y_q1 <- WZ[which(Z > 0 & WZ > mean(WZ) & locmoran[, 4] >= cv)]
-    b_q1 <- b[which(Z > 0 & WZ > mean(WZ) & locmoran[, 4] >= cv)]
-    x_q2 <- Z[which(Z > 0 & WZ < mean(WZ) & locmoran[, 4] <= (-1) * cv)]
-    y_q2 <- WZ[which(Z > 0 & WZ < mean(WZ) & locmoran[, 4] <= (-1) * cv)]
-    b_q2 <- b[which(Z > 0 & WZ < mean(WZ) & locmoran[, 4] <= (-1) * cv)]
-    x_q3 <- Z[which(Z < 0 & WZ < mean(WZ) & locmoran[, 4] >= cv)]
-    y_q3 <- WZ[which(Z < 0 & WZ < mean(WZ) & locmoran[, 4] >= cv)]
-    b_q3 <- b[which(Z < 0 & WZ < mean(WZ) & locmoran[, 4] >= cv)]
-    x_q4 <- Z[which(Z < 0 & WZ > mean(WZ) & locmoran[, 4] <= (-1) * cv)]
-    y_q4 <- WZ[which(Z < 0 & WZ > mean(WZ) & locmoran[, 4] <= (-1) * cv)]
-    b_q4 <- b[which(Z < 0 & WZ > mean(WZ) & locmoran[, 4] <= (-1) * cv)]
+    x_q1 <- x[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] >= cv)]
+    y_q1 <- WX[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] >= cv)]
+    b_q1 <- b[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] >= cv)]
+    labels_q1 <- labels[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] >= cv)]
+    x_q2 <- x[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    y_q2 <- WX[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    b_q2 <- b[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    labels_q2 <- labels[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    x_q3 <- x[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] >= cv)]
+    y_q3 <- WX[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] >= cv)]
+    b_q3 <- b[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] >= cv)]
+    labels_q3 <- labels[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] >= cv)]
+    x_q4 <- x[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    y_q4 <- WX[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    b_q4 <- b[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] <= (-1) * cv)]
+    labels_q4 <- labels[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] <= (-1) * cv)]
   } else {
-    x_q1 <- Z[which(Z > 0 & WZ > mean(WZ) & locmoran[, 4] < cv)]
-    y_q1 <- WZ[which(Z > 0 & WZ > mean(WZ) & locmoran[, 4] < cv)]
-    b_q1 <- b[which(Z > 0 & WZ > mean(WZ) & locmoran[, 4] < cv)]
-    x_q2 <- Z[which(Z > 0 & WZ < mean(WZ) & locmoran[, 4] > (-1) * cv)]
-    y_q2 <- WZ[which(Z > 0 & WZ < mean(WZ) & locmoran[, 4] > (-1) * cv)]
-    b_q2 <- b[which(Z > 0 & WZ < mean(WZ) & locmoran[, 4] > (-1) * cv)]
-    x_q3 <- Z[which(Z < 0 & WZ < mean(WZ) & locmoran[, 4] < cv)]
-    y_q3 <- WZ[which(Z < 0 & WZ < mean(WZ) & locmoran[, 4] < cv)]
-    b_q3 <- b[which(Z < 0 & WZ < mean(WZ) & locmoran[, 4] < cv)]
-    x_q4 <- Z[which(Z < 0 & WZ > mean(WZ) & locmoran[, 4] > (-1) * cv)]
-    y_q4 <- WZ[which(Z < 0 & WZ > mean(WZ) & locmoran[, 4] > (-1) * cv)]
-    b_q4 <- b[which(Z < 0 & WZ > mean(WZ) & locmoran[, 4] > (-1) * cv)]
+    x_q1 <- x[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] < cv)]
+    y_q1 <- WX[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] < cv)]
+    b_q1 <- b[which(x > mean(x) & WX > mean(WX) & locmoran[, 4] < cv)]
+    x_q2 <- x[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] > (-1) * cv)]
+    y_q2 <- WX[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] > (-1) * cv)]
+    b_q2 <- b[which(x > mean(x) & WX < mean(WX) & locmoran[, 4] > (-1) * cv)]
+    x_q3 <- x[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] < cv)]
+    y_q3 <- WX[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] < cv)]
+    b_q3 <- b[which(x < mean(x) & WX < mean(WX) & locmoran[, 4] < cv)]
+    x_q4 <- x[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] > (-1) * cv)]
+    y_q4 <- WX[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] > (-1) * cv)]
+    b_q4 <- b[which(x < mean(x) & WX > mean(WX) & locmoran[, 4] > (-1) * cv)]
   }
   
-  lw.lm <- lm(WZ ~ Z)
-  plot(Z, WZ, xlab="X_centred", ylab="WX", pch = 20, cex = 0.33, col = "gray70", xlim = c(min(Z),max(Z)), ylim = c(min(WZ, b),max(WZ, b)))
-  abline(h = mean(WZ), lty = "dashed", col = "grey30")
-  abline(v = 0, lty = "dashed", col = "grey30")
+  lw.lm <- lm(WX ~ x)
+  plot(x, WX, xlab="X", ylab="WX", pch = 20, cex = 0.5, col = "gray70", xlim = c(min(x),max(x)), ylim = c(min(WX, b),max(WX, b)))
+  abline(h = mean(WX), lty = "dashed", col = "grey30")
+  abline(v = mean(x), lty = "dashed", col = "grey30")
   abline(lw.lm, lty = "dotted", col = "grey40")
   
   for(i in 1:length(x_q1)) {
     lines(c(x_q1[i], x_q1[i]), c(y_q1[i], b_q1[i]), type = "l", col = "lightpink", lwd = 1)
     points(c(x_q1[i]), c(y_q1[i]), pch = 20, cex = 0.5, col = "firebrick")
-    if (labs && length(x_q1) > 0) 
-      text(x_q1[i], y_q1[i], labels = labels[i], pos = 2, cex = 0.5, col = "firebrick")
+    if (labs) 
+      text(x_q1[i], y_q1[i], labels = labels_q1[i], pos = 2, cex = 0.5, col = "firebrick")
   }
-      
+
   for(i in 1:length(x_q2)) {
     lines(c(x_q2[i], x_q2[i]), c(y_q2[i], b_q2[i]), type = "l", col = "lightblue1", lwd = 1)
     points(c(x_q2[i]), c(y_q2[i]), pch = 20, cex = 0.5, col = "royalblue")
-    if (labs  && length(x_q2) > 0) 
-      text(x_q2[i], y_q2[i], labels = labels[i], pos = 2, cex = 0.5, col = "royalblue")
+    if (labs) 
+      text(x_q2[i], y_q2[i], labels = labels_q2[i], pos = 2, cex = 0.5, col = "royalblue")
   }
       
   for(i in 1:length(x_q3)) {
     lines(c(x_q3[i], x_q3[i]), c(y_q3[i], b_q3[i]), type = "l", col = "lightpink", lwd = 1)
     points(c(x_q3[i]), c(y_q3[i]), pch = 20, cex = 0.5, col = "firebrick")
-    if (labs  && length(x_q3) > 0) 
-      text(x_q3[i], y_q3[i], labels = labels[i], pos = 2, cex = 0.5, col = "firebrick")
+    if (labs) 
+      text(x_q3[i], y_q3[i], labels = labels_q3[i], pos = 2, cex = 0.5, col = "firebrick")
   }
     
   for(i in 1:length(x_q4)) {
     lines(c(x_q4[i], x_q4[i]), c(y_q4[i], b_q4[i]), type = "l", col = "lightblue1", lwd = 1)
     points(c(x_q4[i]), c(y_q4[i]), pch = 20, cex = 0.5, col = "royalblue")
-    if (labs  && length(x_q4) > 0) 
-      text(x_q4[i], y_q4[i], labels = labels[i], pos = 2, cex = 0.5, col = "royalblue")
+    if (labs) 
+      text(x_q4[i], y_q4[i], labels = labels_q4[i], pos = 2, cex = 0.5, col = "royalblue")
   }
   
   if(return_df) {
-    res <- data.frame(z = Z, wz = WZ, b = b, line_lengths = abs(WZ - b)) #n_sig = length(x_q1) + length(x_q2) + length(x_q3) + length(x_q4)
+    res <- data.frame(region.id = labels, x = x, WX = WX, b = b, line_lengths = abs(WX - b))
     invisible(res)
   }
 }
