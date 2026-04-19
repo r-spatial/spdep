f792540c91538bcd60db486836b839b4a6c3c0bf
René Westerholt <rene.westerholt@web.de>
2026-04-19 13:09:55 +0200

    Update moran.plot.seismogram.R

--- R/moran.plot.seismogram.R
+++ R/moran.plot.seismogram.R
@@ -27,12 +27,12 @@ moran.plot.seismogram <- function(x, listw, locmoran, alpha = 0.05, adjusted_p =
   }
   if (is.null(spChk)) 
     spChk <- get.spChkOption()
-  if (spChk && !chkIDs(x, listw)) 
-    stop("Check of data and weights ID integrity failed")
+  if (spChk && !chkIDs(locmoran, listw)) 
+    stop("Check of locmoran and weights ID integrity failed")
   if (is.null(xlab)) 
     xlab <- xname
   if (is.null(ylab)) 
-    ylab <- paste("spatially lagged", xname)
+    ylab <- paste("spatially lagged centred", xname)
 
   WX <- lag.listw(listw, scale(x, scale = F), zero.policy = zero.policy)
   if (anyNA(WX)) warning("no-neighbour observation(s) found - use zero.policy=TRUE")
@@ -58,7 +58,7 @@ moran.plot.seismogram <- function(x, listw, locmoran, alpha = 0.05, adjusted_p =
   b_q4 <- b[which(x < mean(x) & WX > mean(WX))]
 
   lw.lm <- lm(WX ~ x)
-  plot(x, WX, xlab="X", ylab="WX", pch = 20, cex = 0.5, col = "gray70", xlim = c(min(x),max(x)), ylim = c(min(WX, b),max(WX, b)))
+  plot(x, WX, xlab=xlab, ylab=ylab, pch = 20, cex = 0.5, col = "gray70", xlim = c(min(x),max(x)), ylim = c(min(WX, b),max(WX, b)))
   abline(h = mean(WX), lty = "dashed", col = "grey30")
   abline(v = mean(x), lty = "dashed", col = "grey30")
   abline(lw.lm, lty = "dotted", col = "grey40")
@@ -85,7 +85,7 @@ moran.plot.seismogram <- function(x, listw, locmoran, alpha = 0.05, adjusted_p =
     lines(c(df_q4$x_q4[i], df_q4$x_q4[i+1]), c(df_q4$b_q4[i], df_q4$b_q4[i+1]), type = "l", col = "royalblue")
 
   if(return_df) {
-    res <- data.frame(x = x, WX = WX, b = b)
+    res <- data.frame(labels=as.character(attr(listw, "region.id")), x=x, wx=WX, b=b)
     invisible(res)
   }
 }
